0782eeb54fe674353efe2d9f33e5f8a1a049516f
Ikotun Collins <danlogan2003@gmail.com>
2023-08-02 09:38:58 +0100

    models

--- acasm/demo/models.py
+++ acasm/demo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    content = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+
+
+    
