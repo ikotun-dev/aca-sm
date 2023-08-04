f09522436bcf5d81d419aa559c631ff9edb2b610
Ikotun Collins <danlogan2003@gmail.com>
2023-08-04 18:09:46 +0100

    migrations

--- acasm/demo/urls.py
+++ acasm/demo/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path("home", views.home),
-    ppath("signup", views.Signup.as_view())
-    
+    path("signup", views.Signup.as_view())
+
 ]
